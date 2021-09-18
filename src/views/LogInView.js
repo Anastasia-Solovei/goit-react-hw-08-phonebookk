45a9ff10e57c50fad503791b2cf91a080fbe1072
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-18 17:30:53 +0300

    creates component LogIn view

--- src/views/LogInView.js
+++ src/views/LogInView.js
@@ -1,22 +1,48 @@
 import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import authOperations from "../redux/auth/auth-operations";
+
 import s from "./Views.module.css";
 
 export default function LogInView() {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case "email":
+        return setEmail(value);
+
+      case "password":
+        return setPassword(value);
+
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(authOperations.login({ email, password }));
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className={s.Container}>
       <h2 className={s.FormHeader}>Log In page</h2>
-      <form
-      //</div>onSubmit={handleSubmit}
-      >
+      <form onSubmit={handleSubmit}>
         <label className={s.FormInput}>
           Email
           <input
             type="email"
             name="email"
-            //value={email}
-            //onChange={handleChange}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            value={email}
+            onChange={handleChange}
+            pattern="[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*"
+            title="Email может состоять из букв, тире, цифр и не может содержать пробелы"
             required
           />
         </label>
@@ -26,10 +52,10 @@ export default function LogInView() {
           <input
             type="password"
             name="password"
-            //value={password}
-            //onChange={handleChange}
+            value={password}
+            onChange={handleChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            title="Пароль может состоять из букв, тире, цифр и не может содержать пробелы"
             required
           />
         </label>
