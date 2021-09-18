097305b90f52e7d0bf612bd20f7e8b18c8b3e957
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-18 17:31:37 +0300

    adds AuthNav and UserMenu components to AppBar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import Navigation from "../Navigation";
+import AuthNav from "../AuthNav";
+import UserMenu from "../UserMenu";
+import { authSelectors } from "../../redux/auth";
+
 import s from "./AppBar.module.css";
 
 export default function AppBar() {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   return (
     <header className={s.AppBar}>
       <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </header>
   );
 }
