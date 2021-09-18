afe7debbb1ed3669a920ce531c3e2ed63082f10c
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-18 17:35:35 +0300

    changes initialState, adds authOperations.login.fulfilled, authOperations.logout.fulfilled

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -1,26 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit'
-import authOperations from './auth-operations'
+import { createSlice } from "@reduxjs/toolkit";
+import authOperations from "./auth-operations";
 
 const initialState = {
   user: {
-    name: null,
-    email: null,
-    password: null,
+    name: "",
+    email: "",
+    password: "",
   },
   token: null,
   isLoggedIn: false,
-}
+};
 
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   extraReducers: {
     [authOperations.register.fulfilled](state, action) {
-      // state.user = action.payload.user;
-      // state.token = action.payload.token;
-      // state.isLoggedIn = true;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
+    [authOperations.login.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
+    [authOperations.logout.fulfilled](state, _) {
+      state.user = {
+        name: "",
+        email: "",
+        password: "",
+      };
+      state.token = null;
+      state.isLoggedIn = false;
     },
   },
-})
+});
 
-export default authSlice.reducer
+export default authSlice.reducer;
