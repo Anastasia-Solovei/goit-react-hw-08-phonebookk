fb086ff68cd0ccf551d1013e83d94f294cc6bca7
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-18 17:29:33 +0300

    updates contacts operations

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -1,35 +1,34 @@
-import { createAsyncThunk } from '@reduxjs/toolkit'
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import {
   fetchContactsAction,
   addContactAction,
   deleteContactAction,
-} from './contacts-actions'
-import axios from 'axios'
-console.dir(axios)
-// axios.defaults.baseURL = "http://localhost:4040";
+} from "./contacts-actions";
+import axios from "axios";
+console.dir(axios);
 
 export const fetchContacts = createAsyncThunk(
   fetchContactsAction,
   async (_, { rejectWithValue }) => {
     try {
-      const contacts = await axios.get('/contacts')
-      console.log(contacts)
-      return contacts.data
+      const contacts = await axios.get("/contacts");
+      console.log(contacts);
+      return contacts.data;
     } catch (error) {
-      return rejectWithValue(error)
+      return rejectWithValue(error);
     }
-  },
-)
+  }
+);
 
 export const addContact = createAsyncThunk(addContactAction, async (item) => {
-  const contact = await axios.post('/contacts', item)
-  return contact.data
-})
+  const contact = await axios.post("/contacts", item);
+  return contact.data;
+});
 
 export const deleteContact = createAsyncThunk(
   deleteContactAction,
   async (contactId) => {
-    await axios.delete(`/contacts/${contactId}`)
-    return contactId
-  },
-)
+    await axios.delete(`/contacts/${contactId}`);
+    return contactId;
+  }
+);
