80a7861f8b30bb8ccce53c5e0554469f1d16452b
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-18 17:34:17 +0300

    refactoring of auth operations

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,25 +1,50 @@
-import axios from 'axios'
-import { createAsyncThunk } from '@reduxjs/toolkit'
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import {
+  usersSignUpAction,
+  usersLoginAction,
+  usersLogoutAction,
+} from "./auth-actions";
+import axios from "axios";
+axios.defaults.baseURL = "https://connections-api.herokuapp.com";
+console.log(axios.defaults);
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com'
-console.log(axios.defaults)
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = "";
+  },
+};
 
-const register = createAsyncThunk('auth/register', async (credentials) => {
+const register = createAsyncThunk(usersSignUpAction, async (credentials) => {
   try {
-    const { data } = await axios.post('/users/signup', credentials)
-    console.log(data)
-    return data
-    //token.set(data.token);
+    const { data } = await axios.post("/users/signup", credentials);
+    console.log(data);
+    token.set(data.token);
+    return data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
-export default { register }
+const login = createAsyncThunk(usersLoginAction, async (credentials) => {
+  try {
+    const { data } = await axios.post("/users/login", credentials);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-// const login = createAsyncThunk("auth/login", async (credentials) => {
-//   try {
-//     const { data } = await axios.post("/users/login", credentials);
-//     //token.set(data.token);
-//   } catch {}
-// });
+const logout = createAsyncThunk(usersLogoutAction, async () => {
+  try {
+    await axios.post("/users/logout");
+    token.unset();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export default { register, login, logout };
