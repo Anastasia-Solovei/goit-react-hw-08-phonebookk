1c11eb6ca9c8006794dc5cb859f42b20cad3f563
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-18 17:34:39 +0300

    refactoring of auth selectors

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,3 +1,5 @@
-export const getIsLoggedIn = (state) => state.auth.isLoggedIn;
+const getIsLoggedIn = (state) => state.auth.isLoggedIn;
 
-export const getUserName = (state) => state.auth.user.name;
+const getUserName = (state) => state.auth.user.name;
+
+export default { getIsLoggedIn, getUserName };
